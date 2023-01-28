d77017406a790ed89d013696a0ca7ad1e7c68103
Connor <connordupuis760@gmail.com>
2023-01-28 01:13:56 -0700

    Map autosizes to screen size

--- src/pages/Maps.tsx
+++ src/pages/Maps.tsx
@@ -27,33 +27,53 @@ const buildTarkovMapMenuItems = (): DropdownElement[] => {
 const Maps = () => {
     const { map = "", subMap = "" } = useParams();
 
+    useEffect(() => {
+        let viewableHeight = window.innerHeight - 65;
+        if (viewableHeight < 100) {
+            viewableHeight = window.innerHeight;
+        }
+
+        document.documentElement.style.setProperty(
+            "--display-height",
+            `${viewableHeight}px`
+        );
+
+        const cleanup = () => {
+            document.documentElement.style.setProperty(
+                "--display-height",
+                `auto`
+            );
+        };
+
+        return cleanup;
+    });
+
     const ref = useRef<ReactZoomPanPinchRef>(null);
 
     useEffect(() => {
-        setTimeout(() => {
-            ref?.current?.centerView(0.5, 500, "easeOut");
-        }, 300);
+        ref?.current?.resetTransform();
     }, [subMap]);
 
     return (
         <>
             <DropdownMenu navData={buildTarkovMapMenuItems()} />
 
-            <div className="image-wrapper">
+            <div className="display-wrapper">
                 <TransformWrapper
                     ref={ref}
-                    minScale={0.1}
-                    initialScale={0.5}
-                    centerZoomedOut
+                    initialScale={1}
                     centerOnInit
+                    wheel={{ step: 0.2 }}
                 >
-                    <TransformComponent wrapperClass="transform-component">
-                        <img
-                            width="100%"
-                            src={tarkovMaps[map].subMaps[subMap]}
-                            alt={"Map"}
-                            loading="eager"
-                        />
+                    <TransformComponent>
+                        <div className="map-image-wrapper">
+                            <img
+                                alt={"Map"}
+                                loading="lazy"
+                                className="map-image"
+                                src={tarkovMaps[map].subMaps[subMap]}
+                            />
+                        </div>
                     </TransformComponent>
                 </TransformWrapper>
             </div>
