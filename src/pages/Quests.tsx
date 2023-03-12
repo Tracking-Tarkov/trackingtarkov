a12d84508bd3a6cf46c758fc65e04db4e8ad6829
Zach Fuchs <zachef221@gmail.com>
2023-03-12 03:43:02 -0600

    Remove unused import

--- src/pages/Quests.tsx
+++ src/pages/Quests.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef, useCallback } from "react";
+import { useEffect, useRef, useCallback } from "react";
 import ReactFlow, {
     ReactFlowProvider,
     ConnectionLineType,
